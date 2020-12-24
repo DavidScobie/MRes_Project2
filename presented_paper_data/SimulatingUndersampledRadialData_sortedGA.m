bc9ce89bcee50c38c9125713bce1a1b8d287ecd6
David Scobie <david.scobie.20@ucl.ac.uk>
2020-12-24 10:48:43 +0000

    adding original SimulatingUndersampledRadialData_sortedGA.m and resample_undersample_data.m to repo

---
+++ presented_paper_data/SimulatingUndersampledRadialData_sortedGA.m
@@ -0,0 +1,45 @@
+function [image_out] = SimulatingUndersampledRadialData_sortedGA(image_in, acc_fact)
+ 
+%close all;
+% addpath 'gridder'
+
+    col_len = size(image_in, 1);
+    phs_len = size(image_in, 3);
+        
+    %----------------------------------------------------------------------
+   %  Calculate predicted traj + weights
+    %----------------------------------------------------------------------
+                                     
+    [trajectory, weights] = CalculateRadialTrajectoryDL_sortedGA(col_len, phs_len, acc_fact);
+    
+%    disp('trajectory calculated');
+    
+    
+        %----------------------------------------------------------------------
+        % Now sample the k-space data with this trajectory
+        %----------------------------------------------------------------------
+
+        k_image_in     = itok(image_in, [1 2]);
+        sampled_data   = grid_data_bck(trajectory, k_image_in, [0 0 1]);   
+    
+%        disp('sampled data calculated');
+        %----------------------------------------------------------------------
+        % Now grid all data
+        %----------------------------------------------------------------------   
+      % this is the bit that fails if phs in trsjectory is set to -phs/2 to
+      % phs/2
+      
+        gridded_k_data   = grid_data(trajectory, sampled_data, weights, [col_len, col_len, phs_len], [0 0 1]);                                  
+%        disp('data regridded');
+    
+        image_out = ktoi(gridded_k_data, [1, 2]);
+
+        % there is some scaling problem i am not sure about.....
+        image_out = image_out / (col_len/(acc_fact*10.0));
+    
+%        figure;imagesc(abs(image_in(:,:,1)))
+%        figure;imagesc(abs(image_out(:,:,1)))
+
+
+%    end
+return
