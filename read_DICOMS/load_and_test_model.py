e712dbbc5107bee5c37296e94136f92c60adeb6c
David Scobie <david.scobie.20@ucl.ac.uk>
2021-04-29 17:34:21 +0100

    can now image any subject any slice. All in big 5D matrices

--- read_DICOMS/load_and_test_model.py
+++ read_DICOMS/load_and_test_model.py
@@ -18,11 +18,6 @@ model.summary()
 fstart = h5py.File('./low_res_data/d1_00001.h5','r')['x']
 print(fstart)
 fstart = tf.expand_dims(fstart, axis=3)
-# f2 = h5py.File('./low_res_data/d1_00002.h5','r')['x']
-# print(f2)
-# stck = tf.stack([fstart,f2], axis=3)
-# print(stck)
-# empty = tf.zeros([20, 192, 192, 12], tf.float32)
 
 for i in range(11):
     print(i)
@@ -31,29 +26,29 @@ for i in range(11):
     fi = tf.expand_dims(fi, axis=3)
     fnext = tf.concat([fstart,fi], axis=3)
     fstart = fnext
-    # f_dataset = 'frame_%04d_%03d' % (iSbj, idx_frame)
-print(fnext)
+# print(fnext)
+fnext = tf.transpose(fnext, perm=[3, 0, 1, 2])
+# print(fnext)
 
-'''
-bigger_dset = tf.expand_dims(dset, axis=0)
-bigger_dset = tf.expand_dims(bigger_dset, axis=4)
+# bigger_dset = tf.expand_dims(dset, axis=0)
+bigger_dset = tf.expand_dims(fnext, axis=4)
 
 low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset, tf.float32))
+print(low_res)
 plt.figure(0)
-plt.imshow(low_res[0,:,:], cmap='gray')
+plt.imshow(low_res[3,5,:,:], cmap='gray')
 
 y_pred = model.predict(bigger_dset)
 print(y_pred.shape)
 
 test_pred = tf.squeeze(tf.image.convert_image_dtype(y_pred, tf.float32))
-print(low_res)
 plt.figure(1)
-plt.imshow(test_pred[0,:,:], cmap='gray')
+plt.imshow(test_pred[3,5,:,:], cmap='gray')
 
 
 # PlotUtils.plotVid(img,vmin=0,vmax=1,axis=0)
 
 plt.show()
-'''
+
 
 
