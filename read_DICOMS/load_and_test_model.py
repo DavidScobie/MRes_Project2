354133cc125d974c7fd4549a4d7e0caa15dc7529
David Scobie <david.scobie.20@ucl.ac.uk>
2021-04-29 17:06:12 +0100

    finally concatenated tensors into stack of 12

--- read_DICOMS/load_and_test_model.py
+++ read_DICOMS/load_and_test_model.py
@@ -15,19 +15,32 @@ model.summary()
 
 # filename = './low_res_data/d1_00001.h5'
 # k = tf.keras.utils.HDF5Matrix(filename)
-f = h5py.File('./low_res_data/d1_00001.h5','r')
-keys = f.keys()
-print(keys)
-dset = f['x']
-print(dset)
+fstart = h5py.File('./low_res_data/d1_00001.h5','r')['x']
+print(fstart)
+fstart = tf.expand_dims(fstart, axis=3)
+# f2 = h5py.File('./low_res_data/d1_00002.h5','r')['x']
+# print(f2)
+# stck = tf.stack([fstart,f2], axis=3)
+# print(stck)
+# empty = tf.zeros([20, 192, 192, 12], tf.float32)
 
+for i in range(11):
+    print(i)
+    filepath = './low_res_data/d1_%05d.h5' % (i+2)
+    fi = h5py.File(filepath,'r')['x']
+    fi = tf.expand_dims(fi, axis=3)
+    fnext = tf.concat([fstart,fi], axis=3)
+    fstart = fnext
+    # f_dataset = 'frame_%04d_%03d' % (iSbj, idx_frame)
+print(fnext)
 
+'''
 bigger_dset = tf.expand_dims(dset, axis=0)
 bigger_dset = tf.expand_dims(bigger_dset, axis=4)
 
 low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset, tf.float32))
 plt.figure(0)
-plt.imshow(low_res[7,:,:], cmap='gray')
+plt.imshow(low_res[0,:,:], cmap='gray')
 
 y_pred = model.predict(bigger_dset)
 print(y_pred.shape)
@@ -35,46 +48,12 @@ print(y_pred.shape)
 test_pred = tf.squeeze(tf.image.convert_image_dtype(y_pred, tf.float32))
 print(low_res)
 plt.figure(1)
-plt.imshow(test_pred[7,:,:], cmap='gray')
+plt.imshow(test_pred[0,:,:], cmap='gray')
 
 
 # PlotUtils.plotVid(img,vmin=0,vmax=1,axis=0)
 
-# kev = open("low_res_data.csv","w+")
-
-# # save numpy array as csv file
-# from numpy import asarray
-# from numpy import savetxt
-# # define data
-# data = asarray([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]])
-# # save to csv file
-# savetxt('low_res_data.csv', low_res, delimiter=',')
-
-# def iter_3D(low_res):
-#     for i in range(low_res.shape[0]):
-#         for j in range(low_res.shape[1]):
-#             for k in range(low_res.shape[2]):
-#                 yield i, j, k
-
-# l = []
-
-# for i, j, k in iter_3D(low_res):
-#     l.append('%d %d %d %d' %(str(indices_x(i, j, k)), str(indices_y(i, j, k)), str(indices_z(i, j, k)), str(low_res[i, j, k])))
-
-# with open('file_1.csv', 'w') as f:
-#     f.write("\n".join(l))
-
-# import numpy as np
-# import pandas as pd
-# from pandas import Panel
-# # create an example array
-# a = np.arange(24).reshape([2,3,4])
-# # convert it to stacked format using Pandas
-# stacked = pd.Panel(low_res.swapaxes(1,2)).to_frame().stack().reset_index()
-# stacked.columns = ['x', 'y', 'z', 'value']
-# # save to disk
-# stacked.to_csv('stacked.csv', index=False)
-
 plt.show()
+'''
 
 
