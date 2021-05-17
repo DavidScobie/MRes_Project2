f48eff118b7df86a3eae7970e6171ad55000c18c
David Scobie <david.scobie.20@ucl.ac.uk>
2021-05-17 11:00:09 +0100

    load and test model cropped result down to 128x128

--- read_DICOMS/load_and_test_model.py
+++ read_DICOMS/load_and_test_model.py
@@ -11,17 +11,17 @@ import scipy.io as sio
 
 # load model
 # model = load_model('./old_network/model.h5')
-model = load_model('./fi_paper_redo_7_5_21/model.h5')
+model = load_model('./fi_e_150_192_jen_preproc_split_s_a_p/model.h5')
 # summarize model.
 model.summary()
 
 #Read in the first dataset of 12
-fstart = h5py.File('./low_res_data_128/d1_00001.h5','r')['x']
+fstart = h5py.File('./low_res_data/d1_00001.h5','r')['x']
 fstart = tf.expand_dims(fstart, axis=3)
 
 #Read in and concatenate the next 11 datasets
 for i in range(11):
-    filepath = './low_res_data_128/d1_%05d.h5' % (i+2)
+    filepath = './low_res_data/d1_%05d.h5' % (i+2)
     fi = h5py.File(filepath,'r')['x']
     fi = tf.expand_dims(fi, axis=3)
     fnext = tf.concat([fstart,fi], axis=3)
@@ -40,19 +40,20 @@ low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset, tf.float32))
 y_pred = model.predict(bigger_dset)
 
 test_pred = tf.squeeze(tf.image.convert_image_dtype(y_pred, tf.float32))
-# plt.figure(1)
-# plt.imshow(test_pred[3,5,:,:], cmap='gray')
+print(tf.shape(test_pred))
 
+
+#cropping the 192 down to 128
+test_pred = test_pred[:,:,32:160,32:160]
+print(tf.shape(test_pred))
 
 # turn the tensorflow arrays into numpy arrays
 low_res_np = tf.make_ndarray(tf.make_tensor_proto(low_res))
 test_pred_np = tf.make_ndarray(tf.make_tensor_proto(test_pred))
 
 #Plot the reconstructed frames as an animation
-PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0,savepath='.\model_128_same_as_pap')
-
-#savepath example
-# PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0,savepath='.\old_network\original')
+PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0,savepath='.\model_192_same_as_pap_crop_to_128')
+# PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0)
 
 #save the matrices to files
 # sio.savemat('DICOM_and_reconstruction.mat',{'low_res_DICOM':low_res_np, 'reconstruction':test_pred_np}) #you can save as many arrays as you want
