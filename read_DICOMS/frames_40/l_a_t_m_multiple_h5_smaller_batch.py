4ace789ac342217205014793dff3412f6c959eb4
DavidScobie <david.scobie.20@ucl.ac.uk>
2021-09-10 11:32:28 +0100

    low res data made along with model recon data

--- read_DICOMS/frames_40/l_a_t_m_multiple_h5_smaller_batch.py
+++ read_DICOMS/frames_40/l_a_t_m_multiple_h5_smaller_batch.py
@@ -36,26 +36,26 @@ model.summary()
 fstart = h5py.File(os.path.join(filepath, 'd1_00001.h5'),'r')['x']
 fstart = tf.expand_dims(fstart, axis=3)
 
-"""
+
 #get the low resolution data stacked together
 
 for i in range(number_files - 1):
     filepath2 = os.path.join(filepath,'d1_%05d.h5') % (i+2)
     fi = h5py.File(filepath2,'r')['x']
     fi = tf.expand_dims(fi, axis=3)
-    fnext = tf.concat([fstart,fi], axis=3)
-    fstart = fnext
+    fnext_lowres = tf.concat([fstart,fi], axis=3)
+    fstart = fnext_lowres
 
 #Permute dimensions
-fnext = tf.transpose(fnext, perm=[3, 0, 1, 2])
+fnext_lowres = tf.transpose(fnext_lowres, perm=[3, 0, 1, 2])
 
 #expand dimensions
-bigger_dset = tf.expand_dims(fnext, axis=4)
+bigger_dset_lowres = tf.expand_dims(fnext_lowres, axis=4)
 
-#Image one of the slices
-low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset, tf.float32))
+#Squeeze to make image
+low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset_lowres, tf.float32))
 
-"""
+
 
 #Get all of the data to be put through the model and then stacked aferwards
 
@@ -63,12 +63,10 @@ for i in range(number_files - 1):
     filepath2 = os.path.join(filepath,'d1_%05d.h5') % (i+2)
     fi = h5py.File(filepath2,'r')['x']
     fi = tf.expand_dims(fi, axis=3)
-    print('fi',tf.shape(fi),fi.dtype)
 
     fnext = tf.transpose(fi, perm=[3, 0, 1, 2])
     bigger_dset = tf.expand_dims(fnext, axis=4)
     y_pred = model.predict(bigger_dset)
-    print('y_pred',tf.shape(y_pred),y_pred.dtype)
 
     if i == 0:
         y_pred1 = y_pred
@@ -76,34 +74,15 @@ for i in range(number_files - 1):
     y_pred1 = fnext
 
 
-
-    
-print('fnext',tf.shape(fnext),fnext.dtype)
-# fstart = fnext
-
-#Permute dimensions
-# print('fnext',tf.shape(fnext),fnext.dtype)
-
-# print('perm fnext', tf.shape(fnext),fnext.dtype)
-
-#expand dimensions
-
-# print('bigger_dset',tf.shape(bigger_dset),bigger_dset.dtype)
-
-
-
+# convert y pred to float32 type
 test_pred = tf.squeeze(tf.image.convert_image_dtype(fnext, tf.float32))
 
 
 #cropping the 192 down to 128
 # test_pred = test_pred[:,:,32:160,32:160]
 
-#Image one of the slices
-# low_res = tf.squeeze(tf.image.convert_image_dtype(bigger_dset, tf.float32))
-
-
 # turn the tensorflow arrays into numpy arrays
-# low_res_np = tf.make_ndarray(tf.make_tensor_proto(low_res))
+low_res_np = tf.make_ndarray(tf.make_tensor_proto(low_res))
 test_pred_np = tf.make_ndarray(tf.make_tensor_proto(test_pred))
 
 #Plot the reconstructed frames as an animation
@@ -111,10 +90,7 @@ test_pred_np = tf.make_ndarray(tf.make_tensor_proto(test_pred))
 # PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0)
 PlotUtils.plotVid(np.squeeze(test_pred_np[6,:,:,:]),vmin=0,vmax=1,axis=0)
 
-#save the matrices to files
-# # sio.savemat('prosp1_DICOM_MSErecon_RDSSIMrecon.mat',{'low_res_DICOM':low_res_np, 'MSE_recon':test_pred_np,  'RDSSIM_recon':test_pred_np_2}) #you can save as many arrays as you want
-
-# sio.savemat('fi_SAX_rest_and_trans_aug_40_y_only_chk_new_gridder_ex3_ds_new.mat',{'low_res_DICOM':low_res_np, 'model_recon':test_pred_np}) #you can save as many arrays as you want
+sio.savemat('fi_SAX_rest_and_trans_aug_40_y_only_chk_new_gridder_ex3_ds_new.mat',{'low_res_DICOM':low_res_np, 'model_recon':test_pred_np}) #you can save as many arrays as you want
 
 plt.show()
 
